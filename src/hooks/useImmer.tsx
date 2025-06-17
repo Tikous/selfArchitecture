e09ba929d778f2e956ba65dc3ece7873ee7c34de
wushengke <extern.shengke.wu@audi.com.cn>
2025-06-17 17:13:38 +0800

    feat: 引入immer

---
+++ src/hooks/useImmer.tsx
@@ -0,0 +1,35 @@
+// 写hooks常用的几个api，useMemo useCallback useState
+
+import { useCallback, useState } from "react"
+import { Draft, freeze, produce } from 'immer'
+
+export type DraftFunction<S> = (draft: Draft<S>) => void
+export type Updater<S> = (arg: S | DraftFunction<S>) => void
+export type ImmerHook<S> = [S, Updater<S>]
+export function useImmer<S = unknown>(initialValue: S | (() => S)) : ImmerHook<S>
+
+export function useImmer<T>(initialValue: T) {
+  const [state, setState] = useState(
+    // 使用freeze冻结对象，防止原始对象被修改
+    freeze(typeof initialValue === 'function' ? initialValue() : initialValue, true)
+  )
+
+  return [state, useCallback((updater: T | DraftFunction<T>) => {
+    if (typeof updater === 'function') {
+      setState(produce(updater as DraftFunction<T>))
+    } else {
+      setState(freeze(updater))
+    }
+  }, [])]
+}
+
+// const [state, setState] = useImmer({
+//   a: 123
+// });
+// const [state, setState] = useImmer(function() {
+//   return {a : 123}
+// });
+// setState((darft) => {
+//   darft.a = 456
+// })
+// console.log(state, setState);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,41 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { ApolloProvider } from '@apollo/client';
-import client from '@/abis/apollo';
-import Chat from '@/components/Chat';
+// import { ChakraProvider } from '@chakra-ui/react';
+// import { ApolloProvider } from '@apollo/client';
+// import client from '@abis/apollo';
+// import Chat from '@components/Chat';
 
-function App() {
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <ChakraProvider>
+//         <Chat />
+//       </ChakraProvider>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+import { useImmer } from '@hooks/useImmer';
+import { useState } from 'react';
+// import { useState } from 'react';
+
+const App = () => {
+  const [data, setData] = useImmer({ info: '京程一灯' });
+  console.log('🐻 App component rendered');
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <Chat />
-      </ChakraProvider>
-    </ApolloProvider>
+    <>
+      <h1
+        className="text-4xl text-[#09F]"
+        onClick={() => {
+          // setData({info: '京程一灯'});
+          setData((draft) => {
+            draft.info = '京程一灯 info'
+          });
+        }}
+      >
+        {data.info}
+      </h1>
+    </>
   );
-}
-
+};
+App.whyDidYouRender = true; // Enable WDYR for this component
 export default App;
